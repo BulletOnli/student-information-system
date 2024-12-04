cb3baa258f754273a85f074e037b69eef11e8c06
Gemmuel <gemmueldelacruz@gmail.com>
2024-12-04 16:01:19 +0800

    dynamic route handler

--- src/app/api/courses/route.ts
+++ src/app/api/courses/route.ts
@@ -1,5 +1,7 @@
 import { getAllCourses } from "@/data-access/course";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(req: Request) {
   const courses = await getAllCourses();
 
